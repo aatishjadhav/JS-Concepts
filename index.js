679314a79757d95515c854acd059503adb9b978e
Atish <atishjadhav835@gmail.com>
2025-04-30 09:03:51 +0530

    closures concept done

--- index.js
+++ index.js
@@ -234,3 +234,30 @@
 // }
 // runTasks();
 
+// Closures
+// A closure is the combination of a function and the lexical environment within which that function was declared.
+// function outerFunction() {
+//   let outerVariable = "Red";
+
+//   function innerFunction() {
+//     console.log(outerVariable);
+//   }
+//   return innerFunction;
+// }
+
+// let closureFunc = outerFunction();
+// closureFunc();
+
+// function counter() {
+//   let count = 0;
+
+//   function innerCountFunc() {
+//     count++;
+//     console.log(count);
+//   }
+//   return innerCountFunc;
+// }
+
+// let myCounter = counter();
+// myCounter();
+// myCounter();
